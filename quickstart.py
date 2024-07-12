022999ca064b7024d0ed8117dee6d7da960e012e
TheronBoozer <boozerwt@gmail.com>
2024-07-12 12:49:27 -0400

    clearing a google form

--- quickstart.py
+++ quickstart.py
@@ -2,7 +2,7 @@ from apiclient import discovery
 from httplib2 import Http
 from oauth2client import client, file, tools
 
-SCOPES = "https://www.googleapis.com/auth/forms.body"
+SCOPES = 'https://www.googleapis.com/auth/forms.body'
 DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
 
 store = file.Storage("token.json")
@@ -20,11 +20,11 @@ form_service = discovery.build(
 )
 
 # Request body for creating a form
-NEW_FORM = {
-    "info": {
-        "title": "Quickstart form",
-    }
-}
+# NEW_FORM = {
+#     "info": {
+#         "title": "Quickstart form",
+#     }
+# }
 
 # Request body to add a multiple-choice question
 NEW_QUESTION = {
@@ -59,15 +59,37 @@ NEW_QUESTION = {
 }
 
 # Creates the initial form
-result = form_service.forms().create(body=NEW_FORM).execute()
+# result = form_service.forms().create(body=NEW_FORM).execute()
 
 # Adds the question to the form
 question_setting = (
     form_service.forms()
-    .batchUpdate(formId=result["formId"], body=NEW_QUESTION)
+    .batchUpdate(formId="1S0D8c-8_oXHXEF687RoDfXnxxnqM9ZLXJHNiLlparqo", body=NEW_QUESTION)
     .execute()
 )
 
+
+
 # Prints the result to show the question has been added
-get_result = form_service.forms().get(formId=result["formId"]).execute()
+get_result = form_service.forms().get(formId="1S0D8c-8_oXHXEF687RoDfXnxxnqM9ZLXJHNiLlparqo").execute()
 print(get_result)
+
+
+def clear_form(form_id):
+    questions = form_service.forms().get(formId=form_id).execute()
+    questions = questions.get('items', [])
+
+    print (questions)
+
+    delete_requests = []
+
+    for i, item in enumerate(questions):
+        if 'title' in item and 'questionItem' in item:
+            delete_requests.append({"deleteItem": {"location": {"index": i}}})
+    delete_requests.reverse()
+
+    batch_delete_request = {"requests" : delete_requests}
+
+    form_service.forms().batchUpdate(formId=form_id, body=batch_delete_request).execute()
+    
+clear_form("1S0D8c-8_oXHXEF687RoDfXnxxnqM9ZLXJHNiLlparqo")
