276efa65baa98cf927a70c8c99e0f75e163c5ae3
TheronBoozer <theronboozer@westminster.net>
2024-08-28 19:02:23 -0400

    something changed?

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# from Function_Phases.Helpers import *
 
-from Function_Phases.Information_Scraping import get_weekly_information
-from Function_Phases.Initiation import send_out_initial_form
-from Function_Phases.Confirmation import create_session_pairings
+# from Function_Phases.Information_Scraping import get_weekly_information
+# from Function_Phases.Initiation import send_out_initial_form
+# from Function_Phases.Confirmation import create_session_pairings
 
-from Scheduled_Entities.Google_Sheet import Google_Sheet
-
+from Scheduler import assign_task_timing
 
 
 
@@ -17,15 +15,9 @@ def main():
 
     # create_session_pairings()
 
-    test = Google_Sheet("https://docs.google.com/spreadsheets/d/1ueTWu_qzz_CshyQsz3_J6j2yVMNs1mIqjJOmqu3KzC0/edit?gid=0#gid=0")
-    test.append([[1,2,3], [4,5,6]])
+    assign_task_timing()
 
 
-
-
-
-
-    
 
 
 
