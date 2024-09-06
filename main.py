98778aeceb5255ff4bf2230e7aae0b43639e7a2f
TheronBoozer <theronboozer@westminster.net>
2024-09-06 18:26:39 -0400

    startup seemingly works now

--- main.py
+++ main.py
@@ -18,19 +18,22 @@ def main():
 def add_to_startup(file_path=""):
     USER_NAME = getpass.getuser()
     bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
-    bat = Path(bat_path + "\\" + "open.bat")
+    bat = Path(bat_path + "\\" + "DM_Bot.bat")
 
     if bat.exists():
         return
     
     if file_path == "":
         file_path = os.path.dirname(os.path.realpath(__file__))
-    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
-        bat_file.write(r'start "" "%s"' % file_path)
+    with open(bat_path + '\\' + "DM_bot.bat", "w+") as bat_file:
+        bat_file.write(r'''@echo off
+cd %s
+python main.py
+pause''' % file_path)
 
 
 def update_tokens():
-    access_form = Google_Form("https://docs.google.com/spreadsheets/d/1PtMRoHYLS_VMHVLC0O4lCinp1BIVL-425-z9owAa88U/edit?resourcekey=&gid=553428416#gid=553428416")
+    Google_Form("https://docs.google.com/spreadsheets/d/1PtMRoHYLS_VMHVLC0O4lCinp1BIVL-425-z9owAa88U/edit?resourcekey=&gid=553428416#gid=553428416")
 
 
 # update_tokens()
