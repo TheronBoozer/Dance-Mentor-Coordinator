d612c5dc522e191cafca0496ea42060f1ee0f3af
TheronBoozer <theronboozer@westminster.net>
2025-02-24 15:44:10 -0500

    Error checking update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from Phases.Information_Scraping import get_weekly_information
 from Phases.Initiation import send_out_initial_form
 from Phases.Confirmation import create_session_pairings
@@ -15,8 +17,8 @@ def main():
     """
     Adds the created tasks to the correctly scheduled times
     """
-    
-    print(f"running on: {os.name};\n\t - 'nt' = Windows\n\t - 'posix' = Linux\n\n")
+
+    print(f"{datetime.now()}:\n\trunning on {os.name}")
 
     task = ''
     if len(sys.argv) > 1:
@@ -25,40 +27,41 @@ def main():
     if task == "Scrape":
 
         get_weekly_information()
-        print("Information saved as pickles (file format, not the food)")
+        print("\tInformation saved as pickles (file format, not the food)")
 
     elif task == "Initiate":
 
         send_out_initial_form()
-        print("Form edited with new sessions")
-        print("Emails sent")
+        print("\tForm edited with new sessions")
+        print("\tEmails sent")
                 
 
     elif task == "Confirm":
 
         create_session_pairings()
-        print("Sessions paired between mentors and mentees")
-        print("Emails sent")
+        print("\tSessions paired between mentors and mentees")
+        print("\tEmails sent")
                 
     elif task == "Update":
 
-        print("Updating...\nWill reboot soon")
+        print("\tUpdating...\nWill reboot soon")
         update()
 
     elif task == "Restart":
 
-        print("Will reboot soon")
+        print("\tWill reboot soon")
         reboot()
 
     elif task == "Test":
         test()
-        print("System tested")
+        print("\tSystem tested")
 
     else:
-        print("Please enter a command line argument for what the program should do\n")
+        print("\tPlease enter a command line argument for what the program should do\n")
 
 try:
     main()
 except Exception as e:
-    print(repr(e))
+    print(f"\t{repr(e)}")
     smtp_mailing([], "ERROR OCCURED", repr(e))
+    print("\tError email sent")
